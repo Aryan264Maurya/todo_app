ec7b555f4366e7f2355fdb3b72ba5523530b5f4f
aryanmaurya45 <aryan.maurya2104@gmail.com>
2023-11-22 19:07:05 +0530

    fixed engine

--- data/database.js
+++ data/database.js
@@ -8,15 +8,3 @@ export const connectDB = () => {
     .then((c) => console.log(`Database Connected with ${c.connection.host}`))
     .catch((e) => console.log(e));
 };
-
-
-// Replace '<your-mongodb-uri>' with your actual MongoDB connection URI
-// const uri = '<your-mongodb-uri>';
-
-// mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
-//   .then(() => {
-//     console.log('Connected to MongoDB');
-//   })
-//   .catch((error) => {
-//     console.error('Error connecting to MongoDB:', error.message);
-//   });
